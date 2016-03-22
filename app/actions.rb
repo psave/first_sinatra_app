706536eff9e913db3055bab401da4bfe04f99f1e
psave <paulsave01@gmail.com>
2016-03-21 19:04:38 -0700

    adding ability to create messages

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,30 @@
 get '/' do
   erb :index
 end
+
+get '/messages' do 
+  @messages = Message.all
+  erb :messages
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :new
+  end
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'show'
+end
